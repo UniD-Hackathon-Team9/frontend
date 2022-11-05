06e58127227b6589af200bd68f4da4750c946a31
1unaram <haram620@icloud.com>
2022-11-05 21:02:41 +0900

    fetch: 질문에 대한 값 가중치 상수 값 추가

--- src/interfaces/question.type.ts
+++ src/interfaces/question.type.ts
@@ -1,16 +1,17 @@
 import { Preference } from "./preference.type";
 
 export interface Question {
-    index: number
-    title: string
-    first: Choice
-    second: Choice
+	index: number;
+	title: string;
+	first: Choice;
+	second: Choice;
+	none: Choice;
 }
 type PersonalityWeights = [number, number, number, number, number];
 
 interface Choice {
-    title: string
-    image: string
-    personality: PersonalityWeights,
-    preference: Preference | null
+	title: string;
+	image: string;
+	personality: PersonalityWeights;
+	preference: Preference | null;
 }
