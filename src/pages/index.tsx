1a937759e667f45a8df15280d228ef1ac754af9c
Sooyoung <dannylisa67@gmail.com>
2022-11-05 19:21:54 +0900

    layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/layout/Header'
 
 export default function Home() {
   return (
     <div>
-      {"Promise<Nine>"}
+      <Header />
     </div>
   )
 }
