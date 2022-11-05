a7ee1aa1a6be2a194ec9e1cad967937161d3b874
1unaram <haram620@icloud.com>
2022-11-06 01:22:03 +0900

    feat: initMap

--- src/components/Navermap.tsx
+++ src/components/Navermap.tsx
@@ -28,8 +28,9 @@ export default function Map() {
 					src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=oan7725a3j"
 				></script>
 			</Head>
+
 			<MobileLayout>
-				<div id="map" style={{ width: "100%", height: "100%" }}></div>
+				<div id="map" style={{ width: "80%", height: "50%" }}></div>
 			</MobileLayout>
 		</>
 	);
