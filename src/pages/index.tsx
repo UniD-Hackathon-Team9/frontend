37b849903ee2c8925034ce28b6e170ba29d91603
Sooyoung <dannylisa67@gmail.com>
2022-11-05 19:23:51 +0900

    layout test case

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/layout/Header'
+import MobileLayout from '../components/layout/MobileLayout'
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <MobileLayout>
+        asdassa
+      </MobileLayout>
+      `
     </div>
   )
 }
