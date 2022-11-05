6b4d9f7b6e78a4968b9514232457b36f5aca6209
1unaram <haram620@icloud.com>
2022-11-06 01:03:16 +0900

    feat: navermap 띄우기 성공

--- src/components/Navermap.tsx
+++ src/components/Navermap.tsx
@@ -1,19 +1,35 @@
 import Header from "./layout/Header";
 import MobileLayout from "./layout/MobileLayout";
+import Head from "next/head";
+import { useEffect } from "react";
 
 export default function Map() {
+	const initMap = () => {
+		const map = new naver.maps.Map("map", {
+			center: new naver.maps.LatLng(33.506, 126.4917),
+			zoom: 15,
+			zoomControl: true,
+		});
+		const mapMarker = new naver.maps.Marker({
+			position: new naver.maps.LatLng(33.506, 126.4917),
+			map: map,
+		});
+	};
+	useEffect(() => {
+		initMap();
+	}, []);
+
 	return (
 		<>
 			<Header />
-			<MobileLayout>
-				<div
-					id="map"
-					className="flex w-full -center content-center"
-				></div>
+			<Head>
 				<script
 					type="text/javascript"
 					src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=oan7725a3j"
 				></script>
+			</Head>
+			<MobileLayout>
+				<div id="map" style={{ width: "100%", height: "100%" }}></div>
 			</MobileLayout>
 		</>
 	);
